cca18adb1346a185a0125233fba9c4b8002cfe8f
shaon <aminul007.ais@gmail.com>
2024-02-15 16:06:22 +0600

    added js function

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,11 @@
+// function play(){
+//     const homeScreen = document.getElementById('home-screen')
+//     homeScreen.classList.add('hidden')
+//     const palyGroundSection = document.getElementById('play-ground')
+//     palyGroundSection.classList.remove('hidden')
+// }
+
+function play(){
+    hideElementById('home-screen')
+    showElementById('play-ground')
+}
