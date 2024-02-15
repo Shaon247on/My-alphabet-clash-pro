dd88a79a42440c916662adb87385815fc8d12699
shaon <aminul007.ais@gmail.com>
2024-02-15 16:38:20 +0600

    added getAplhabet function

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -5,7 +5,15 @@
 //     palyGroundSection.classList.remove('hidden')
 // }
 
+function continueGame(){
+    // step - 1: gemerate a random aplhabet
+    const alphabet = getARandomAlphabet();
+    console.log('Your random alphabet is:', alphabet)
+}
+
+
 function play(){
-    hideElementById('home-screen')
-    showElementById('play-ground')
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,12 @@ function showElementById(elementId){
     const element = document.getElementById(elementId)
     element.classList.remove('hidden')
 }
+
+function getARandomAlphabet(){
+    const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber)
+    const alphabet = alphabets[index]
+    return alphabet
+}
+
